e4f56c46230ffbfa5b700a097277a406787e5a0a
Víctor Olmo <victorolmo42@gmail.com>
2026-01-19 12:37:13 -0400

    Determinar si un año es bisiesto v1.0

---
+++ esBisiesto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("INGRESE UN AÑO: ");
+        int year = int.Parse(Console.ReadLine());
+        if (esBisiesto(year))
+        {
+            Console.WriteLine($"{year} ES BISIESTO");
+        }
+        else
+        {
+            Console.WriteLine($"{year} NO ES BISIESTO");
+        }
+    }
+    static bool esBisiesto(int year)
+    {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+        {
+            return true;
+        }
+        return false;
+    }
+}
